d99179dea48851e682bbb422f7a29b4d2e7ba095
david <diurno@B22-B4.lan.tierno.es>
2025-11-27 11:01:31 +0100

    lombok

--- src/main/java/org/ies/tierno/objects/building/model/Apartment.java
+++ src/main/java/org/ies/tierno/objects/building/model/Apartment.java
@@ -1,13 +1,11 @@
 package org.ies.tierno.objects.building.model;
 
+import jdk.jfr.DataAmount;
+
 import java.util.Arrays;
 import java.util.Objects;
-
 public class Apartment {
     private int floor;
     private String door;
     private Owner[] owners;
-
-    public Apartment(int floor, String door, Owner[] owners) {
-    }
 }
