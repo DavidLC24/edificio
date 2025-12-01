db2858ad436993c5bfef2814a2b885aa69343d13
david <diurno@B22-B4.lan.tierno.es>
2025-12-01 11:53:11 +0100

    building metodos finales

---
+++ src/main/java/org/ies/tierno/objects/building/apps/BuildingApp1.java
@@ -0,0 +1,34 @@
+package org.ies.tierno.objects.building.apps;
+
+import org.ies.tierno.objects.building.model.Building;
+import org.ies.tierno.objects.building.readers.BuildingReader;
+
+import java.util.Scanner;
+
+public class BuildingApp1 {
+    private final Scanner scanner;
+    private final BuildingReader buildingReader;
+
+    public BuildingApp1(Scanner scanner, BuildingReader buildingReader) {
+        this.scanner = scanner;
+        this.buildingReader = buildingReader;
+    }
+
+    public void run(){
+        Building building= buildingReader.read();
+        System.out.println("Planta");
+        int floor= scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Puerta");
+        String door= scanner.nextLine();
+
+        var apartment = building.findApartment(floor, door);
+        if (apartment == null) {
+            System.out.println("No se ha encontrado el apartamento");
+        } else {
+            apartment.showInfo();
+        }
+
+        building.showFloorApartments(2);
+    }
+}
